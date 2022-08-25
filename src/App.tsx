623a026555fa9a846128b9e39366b6926814ee6c
H3rmt <stemmer.enrico@gmail.com>
2022-08-26 01:46:53 +0200

    added functioning navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,31 @@
 import './App.css'
 import {AppBar, IconButton, Toolbar, Typography} from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
-import {Component, createRef, MutableRefObject} from "react";
+import React, {useState} from "react";
 import Navbar from "./components/navbar/navbar";
 
-class App extends Component {
-	private readonly navbar: MutableRefObject<Navbar>;
+const App = () => {
+	const [open, setOpen] = useState(false);
 
-	constructor(props: {}) {
-		super(props);
-		// @ts-ignore
-		this.navbar = createRef<Navbar>()
+	let toggle = () => {
+		setOpen(!open)
 	}
 
-	render() {
-		return (<div>
-			<AppBar position="fixed">
-				<Toolbar>
-					<IconButton color="inherit" aria-label="open drawer" edge="start">
-						<MenuIcon onClick={() => {
-							this.navbar.current.toggle()
-						}}/>
-					</IconButton>
-					<Typography variant="h5" noWrap component="div" align="center">
-						Responsive drawer
-					</Typography>
-				</Toolbar>
-			</AppBar>
-			<Navbar ref={this.navbar}/>
-		</div>)
-	}
+	return (<div>
+		<AppBar position="fixed">
+			<Toolbar>
+				<IconButton color="inherit" aria-label="open drawer" edge="start">
+					<MenuIcon onClick={() => {
+						setOpen(!open)
+					}}/>
+				</IconButton>
+				<Typography variant="h5" noWrap component="div" align="center">
+					Responsive drawer
+				</Typography>
+			</Toolbar>
+		</AppBar>
+		<Navbar open={open} toggle={toggle}/>
+	</div>)
 }
 
 export default App
