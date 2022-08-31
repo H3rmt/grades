8fc6c76840fd5afeded803cb63c0d24a7625dc76
H3rmt <stemmer.enrico@gmail.com>
2022-09-01 01:06:25 +0200

    default Pages for Analysis and Overview

---
+++ src/Analysis/Analysis.tsx
@@ -0,0 +1,13 @@
+import React, {useState} from 'react';
+
+function Analysis() {
+	const [open, setOpen] = useState(false);
+
+	return (<div>
+		Analysis<br/>
+		Analysis<br/>
+		Analysis<br/>
+	</div>);
+}
+
+export default Analysis;
