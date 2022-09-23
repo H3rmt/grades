a9b27a6ca20f91ed5ea3f41f543b554390d65f8b
H3rmt <stemmer.enrico@gmail.com>
2022-09-23 16:54:51 +0200

    tested some toast functions

--- src/components/NewGradeModal/NewGradeModal.tsx
+++ src/components/NewGradeModal/NewGradeModal.tsx
@@ -1,37 +1,24 @@
 import React, {ChangeEvent, useEffect, useState} from 'react';
 import {
-	Alert,
-	AlertTitle,
 	Button,
 	Dialog,
 	DialogActions,
 	DialogContent,
 	DialogTitle,
 	Grid,
-	IconButton,
 	MenuItem,
 	Paper,
 	Select,
 	SelectChangeEvent,
 	Slider,
-	Snackbar,
 	TextField,
 	Typography
 } from "@mui/material";
-import CloseIcon from '@mui/icons-material/Close';
 import {invoke} from "@tauri-apps/api/tauri";
-
-type Subject = {
-	id: number
-	name: string
-	color: string
-}
-
-type Type = {
-	id: number
-	name: string
-	color: string
-}
+import {loadTypes} from "./load";
+import {Subject} from "../../entity/subject";
+import {Type} from "../../entity/type";
+import {toastMessage, useToast} from "../../utils";
 
 function NewGradeModal(props: { open: boolean, closeModal: () => void }) {
 	const [grade, setGrade] = useState(12)
@@ -39,6 +26,7 @@ function NewGradeModal(props: { open: boolean, closeModal: () => void }) {
 	const [type, setType] = useState("")
 	const [info, setInfo] = useState("")
 
+	const [openToast, closeToast] = useToast()
 
 	const [toast, setToast] = useState<string | undefined>(undefined)
 	const [toastOpen, setToastOpen] = useState(false)
@@ -96,10 +84,29 @@ function NewGradeModal(props: { open: boolean, closeModal: () => void }) {
 
 	const getTypes = () => {
 		// @ts-ignore
-		invoke("get_types_js").then((data: string) => {
-			console.log(data)
-			setTypes(JSON.parse(data))
+		loadTypes().then((data) => {
+			setTypes(data)
+		}).catch(() => {
+			setTypes([])
+			// TODO display error
 		})
+
+		const action = (id: any) => (<div style={{ gap: 1 }}>
+			<Button variant="contained" color="secondary" onClick={() => {
+
+			}}>
+				Undo
+			</Button>
+			<Button variant="contained" color="secondary" onClick={() => {
+				closeToast(id)
+			}}>
+				Dismiss
+			</Button>
+		</div>);
+
+		let key = openToast(toastMessage("success", "TESTMESSAGE", {
+			action: action
+		}))
 	}
 
 	useEffect(() => {
@@ -108,68 +115,43 @@ function NewGradeModal(props: { open: boolean, closeModal: () => void }) {
 	}, [props.open])
 
 
-	return (<><Dialog open={props.open} onClose={props.closeModal}>
-				<DialogTitle>Neue Note</DialogTitle>
-				<DialogContent>
-					<Paper elevation={4} variant="elevation" sx={{paddingTop: 1}}>
-						<Grid container padding={1}>
-							<Grid item xs={6} gap={2} padding={2} paddingTop={0}>
-								<Typography variant="h6" fontWeight="normal" paddingBottom={1.5}>Subject</Typography>
-								<Select value={subject} margin="none" onChange={handleSubjectSelectChange} fullWidth>
-									{subjects.map((subject) => {
-										return <MenuItem sx={{color: subject.color}} value={subject.id}>{subject.name}</MenuItem>
-									})}
-								</Select>
-							</Grid>
-							<Grid item xs={6} gap={2} padding={2} paddingTop={0}>
-								<Typography variant="h6" fontWeight="normal" paddingBottom={1.5}>Type</Typography>
-								<Select value={type} margin="none" onChange={handleTypeSelectChange} fullWidth>
-									{types.map((type) => {
-										return <MenuItem sx={{color: type.color}} value={type.id}>{type.name}</MenuItem>
-									})}
-								</Select>
-							</Grid>
-							<Grid item xs={6} gap={2} padding={2} paddingY={0} marginTop={2}>
-								<Typography variant="h6" fontWeight="normal" paddingBottom={0}>Grade</Typography>
-								<TextField value={grade} type="number" fullWidth margin="normal" onChange={handleGradeInputChange}/>
-								<Slider value={grade} color="primary" min={0} max={15} onChange={handleGradeSliderChange}/>
-							</Grid>
-							<Grid item xs={6} gap={2} padding={2} paddingY={0} marginTop={2}>
-								<Typography variant="h6" fontWeight="normal" paddingBottom={0}>Info</Typography>
-								<TextField value={info} type="text" fullWidth margin="normal" onChange={handleInfoInputChange}/>
-							</Grid>
-						</Grid>
-					</Paper>
-				</DialogContent>
-				<DialogActions>
-					<Button onClick={createGrade}>Create</Button>
-				</DialogActions>
-			</Dialog>
-				<Snackbar open={toastOpen} autoHideDuration={8000} anchorOrigin={{vertical: 'bottom', horizontal: 'right'}} onClose={() => {
-					setToastOpen(false)
-				}}>
-					{toast && toast.startsWith("Created") ?
-							<Alert severity="success" variant="outlined" action={
-								<IconButton aria-label="close" size="small" onClick={() => {
-									setToastOpen(false);
-								}}><CloseIcon/>
-								</IconButton>
-							}>{toast}
-								<AlertTitle>Success</AlertTitle>
-							</Alert>
-							:
-							<Alert severity="error" variant="outlined" action={
-								<IconButton aria-label="close" color="inherit" onClick={() => {
-									setToastOpen(false);
-								}}><CloseIcon/>
-								</IconButton>
-							}>{toast}
-								<AlertTitle>Error</AlertTitle>
-							</Alert>
-					}
-				</Snackbar>
-			</>
-	);
+	return (<Dialog open={props.open} onClose={props.closeModal}>
+		<DialogTitle>Neue Note</DialogTitle>
+		<DialogContent>
+			<Paper elevation={4} variant="elevation" sx={{paddingTop: 1}}>
+				<Grid container padding={1}>
+					<Grid item xs={6} gap={2} padding={2} paddingTop={0}>
+						<Typography variant="h6" fontWeight="normal" paddingBottom={1.5}>Subject</Typography>
+						<Select value={subject} margin="none" onChange={handleSubjectSelectChange} fullWidth>
+							{subjects.map((subject) => {
+								return <MenuItem sx={{color: subject.color}} value={subject.id}>{subject.name}</MenuItem>
+							})}
+						</Select>
+					</Grid>
+					<Grid item xs={6} gap={2} padding={2} paddingTop={0}>
+						<Typography variant="h6" fontWeight="normal" paddingBottom={1.5}>Type</Typography>
+						<Select value={type} margin="none" onChange={handleTypeSelectChange} fullWidth>
+							{types.map((type) => {
+								return <MenuItem sx={{color: type.color}} value={type.id}>{type.name}</MenuItem>
+							})}
+						</Select>
+					</Grid>
+					<Grid item xs={6} gap={2} padding={2} paddingY={0} marginTop={2}>
+						<Typography variant="h6" fontWeight="normal" paddingBottom={0}>Grade</Typography>
+						<TextField value={grade} type="number" fullWidth margin="normal" onChange={handleGradeInputChange}/>
+						<Slider value={grade} color="primary" min={0} max={15} onChange={handleGradeSliderChange}/>
+					</Grid>
+					<Grid item xs={6} gap={2} padding={2} paddingY={0} marginTop={2}>
+						<Typography variant="h6" fontWeight="normal" paddingBottom={0}>Info</Typography>
+						<TextField value={info} type="text" fullWidth margin="normal" onChange={handleInfoInputChange}/>
+					</Grid>
+				</Grid>
+			</Paper>
+		</DialogContent>
+		<DialogActions>
+			<Button onClick={createGrade}>Create</Button>
+		</DialogActions>
+	</Dialog>);
 }
 
 export default NewGradeModal
