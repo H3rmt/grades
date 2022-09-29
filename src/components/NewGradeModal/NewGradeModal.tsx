22b9eedb7f44fc5f3e53270029e943d0a2b3a953
H3rmt <stemmer.enrico@gmail.com>
2022-09-29 15:58:20 +0200

    added functionality to extra checkboxes

--- src/components/NewGradeModal/NewGradeModal.tsx
+++ src/components/NewGradeModal/NewGradeModal.tsx
@@ -1,10 +1,13 @@
 import React, {ChangeEvent, useEffect, useState} from 'react';
 import {
 	Button,
+	Checkbox,
 	Dialog,
 	DialogActions,
 	DialogContent,
 	DialogTitle,
+	FormControlLabel,
+	FormGroup,
 	Grid,
 	MenuItem,
 	Paper,
@@ -26,8 +29,10 @@ function NewGradeModal(props: { open: boolean, closeModal: () => void }) {
 	const [grade, setGrade] = useState(12)
 	const [subject, setSubject] = useState("")
 	const [type, setType] = useState("")
-	const [period, setPeriod] = useState("")
+	const [period, setPeriod] = useState("1")  // TODO add setting for default value
 	const [info, setInfo] = useState("")
+	const [notFinal, setNotFinal] = useState(false)
+	const [double, setDouble] = useState(false)
 
 	const toast = useToast()
 
@@ -59,6 +64,14 @@ function NewGradeModal(props: { open: boolean, closeModal: () => void }) {
 		setInfo(event.target.value)
 	}
 
+	const handleNotFinalCheckboxChange = (event: ChangeEvent<HTMLInputElement>) => {
+		setNotFinal(event.target.checked)
+	}
+
+	const handleDoubleCheckboxChange = (event: ChangeEvent<HTMLInputElement>) => {
+		setDouble(event.target.checked)
+	}
+
 	const handleClear = () => {
 		let old = {
 			grade,
@@ -66,6 +79,8 @@ function NewGradeModal(props: { open: boolean, closeModal: () => void }) {
 			type,
 			period,
 			info,
+			notFinal,
+			double
 		}
 
 		setGrade(12)
@@ -73,6 +88,8 @@ function NewGradeModal(props: { open: boolean, closeModal: () => void }) {
 		setType("")
 		setPeriod("")
 		setInfo("")
+		setNotFinal(false)
+		setDouble(false)
 
 		const undo = () => {
 			setGrade(old.grade)
@@ -80,6 +97,8 @@ function NewGradeModal(props: { open: boolean, closeModal: () => void }) {
 			setType(old.type)
 			setPeriod(old.period)
 			setInfo(old.info)
+			setNotFinal(old.notFinal)
+			setDouble(old.double)
 			toastMessage("success", "Undid clear Note window", toast, handleClear)
 		}
 
@@ -148,9 +167,9 @@ function NewGradeModal(props: { open: boolean, closeModal: () => void }) {
 	return (<Dialog open={props.open} onClose={props.closeModal} fullWidth maxWidth="md">
 		<DialogTitle>Neue Note</DialogTitle>
 		<DialogContent>
-			<Paper elevation={4} variant="elevation" sx={{padding: 2}}>
+			<Paper elevation={4} variant="elevation" sx={{padding: 2, marginTop: 2}} square>
 				<Grid container spacing={4} padding={2}>
-					<Grid item xs={12} sm={4} lg={4}>
+					<Grid item xs={12} sm={6} lg={4}>
 						<Stack spacing={2}>
 							<Typography variant="h6" fontWeight="normal">Subject</Typography>
 							<Select value={subject} margin="none" fullWidth onChange={handleSubjectSelectChange}>
@@ -160,7 +179,7 @@ function NewGradeModal(props: { open: boolean, closeModal: () => void }) {
 							</Select>
 						</Stack>
 					</Grid>
-					<Grid item xs={12} sm={4} lg={4}>
+					<Grid item xs={12} sm={6} lg={4}>
 						<Stack spacing={2}>
 							<Typography variant="h6" fontWeight="normal">Type</Typography>
 							<Select value={type} margin="none" fullWidth onChange={handleTypeSelectChange}>
@@ -170,7 +189,7 @@ function NewGradeModal(props: { open: boolean, closeModal: () => void }) {
 							</Select>
 						</Stack>
 					</Grid>
-					<Grid item xs={12} sm={4} lg={4}>
+					<Grid item xs={12} sm={6} lg={4}>
 						<Stack spacing={2}>
 							<Typography variant="h6" fontWeight="normal">Period</Typography>
 							<Select value={period} margin="none" fullWidth onChange={handlePeriodSelectChange}>
@@ -186,27 +205,39 @@ function NewGradeModal(props: { open: boolean, closeModal: () => void }) {
 							</Select>
 						</Stack>
 					</Grid>
-					<Grid item xs={12} sm={12} lg={12}>
+					<Grid item xs={12} sm={6} lg={12}>
 						<Stack spacing={2}>
 							<Typography variant="h6" fontWeight="normal">Grade</Typography>
 							<TextField value={grade} type="number" fullWidth margin="none" onChange={handleGradeInputChange}/>
 							<Slider value={grade} color="secondary" min={0} max={15} onChange={handleGradeSliderChange}/>
 						</Stack>
 					</Grid>
-					<Grid item xs={12} sm={8} lg={8}>
+					<Grid item xs={12} sm={8} lg={9}>
 						<Stack spacing={2} height={1}>
 							<Typography variant="h6" fontWeight="normal">Info</Typography>
-							<TextField multiline value={info} type="text" fullWidth margin="none"
+							<TextField multiline minRows={2} value={info} type="text" fullWidth margin="none"
 										  onChange={handleInfoInputChange}/>
 						</Stack>
 					</Grid>
-
+					<Grid item xs={12} sm={4} lg={3}>
+						<Stack spacing={1.5} height={1}> {/*spacing={1.5} to compensate margin of 1. Checkbox*/}
+							<Typography variant="h6" fontWeight="normal">Extra</Typography>
+							<FormGroup>
+								<FormControlLabel control={
+									<Checkbox color="secondary" checked={notFinal} onChange={handleNotFinalCheckboxChange}/>
+								} label="Not Final"/>
+								<FormControlLabel control={
+									<Checkbox color="secondary" checked={double} onChange={handleDoubleCheckboxChange}/>
+								} label="Count x2"/>
+							</FormGroup>
+						</Stack>
+					</Grid>
 				</Grid>
 			</Paper>
 		</DialogContent>
 		<DialogActions>
 			<Button onClick={handleClear} type="submit" variant="outlined" color="secondary">Clear</Button>
-			<Button onClick={() => props.closeModal()} type="submit" variant="outlined" color="secondary">Close</Button>
+			<Button onClick={props.closeModal} type="submit" variant="outlined" color="secondary">Close</Button>
 			<Button onClick={handleCreateGrade} type="submit" variant="outlined" color="secondary">Create</Button>
 		</DialogActions>
 	</Dialog>);
