6bafd984e02902960ab7ffe65e1d5411d90ca655
H3rmt <stemmer.enrico@gmail.com>
2022-10-03 15:49:20 +0200

    added periods table

--- src-tauri/migrations/src/lib.rs
+++ src-tauri/migrations/src/lib.rs
@@ -4,6 +4,7 @@ mod m20220906_221454_added_types_and_subjects_tables;
 mod m20220907_021039_added_grades_table;
 mod m20220907_022500_added_unique_indexes;
 mod m20220907_201550_added_grade_to_grade;
+mod m20220929_160232_added_periods_table;
 
 pub struct Migrator;
 
@@ -11,10 +12,11 @@ pub struct Migrator;
 impl MigratorTrait for Migrator {
 	fn migrations() -> Vec<Box<dyn MigrationTrait>> {
 		vec![
-			Box::new(m20220906_221454_added_types_and_subjects_tables::Migration),
-			Box::new(m20220907_021039_added_grades_table::Migration),
-			Box::new(m20220907_022500_added_unique_indexes::Migration),
-			Box::new(m20220907_201550_added_grade_to_grade::Migration),
-		]
+            Box::new(m20220906_221454_added_types_and_subjects_tables::Migration),
+            Box::new(m20220907_021039_added_grades_table::Migration),
+            Box::new(m20220907_022500_added_unique_indexes::Migration),
+            Box::new(m20220907_201550_added_grade_to_grade::Migration),
+            Box::new(m20220929_160232_added_periods_table::Migration),
+        ]
 	}
 }
---
+++ src-tauri/migrations/src/m20220929_160232_added_periods_table.rs
@@ -0,0 +1,55 @@
+use sea_orm_migration::prelude::*;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+	async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+		let statement = Table::create()
+				.table(Period::Table).if_not_exists()
+				.col(ColumnDef::new(Period::Id)
+						.integer()
+						.not_null()
+						.auto_increment()
+						.primary_key()
+				)
+				.col(ColumnDef::new(Period::Name)
+						.string()
+						.not_null()
+				)
+				.col(ColumnDef::new(Period::From)
+						.date()
+						.not_null()
+				)
+				.col(ColumnDef::new(Period::To)
+						.date()
+						.not_null()
+				)
+				.to_owned();
+		
+		println!("SQL:{}", statement.to_string(SqliteQueryBuilder));
+		
+		manager.create_table(statement).await
+	}
+	
+	async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+		let statement = Table::drop()
+				.table(Period::Table)
+				.to_owned();
+		
+		println!("SQL:{}", statement.to_string(SqliteQueryBuilder));
+		
+		manager.drop_table(statement).await
+	}
+}
+
+#[derive(Iden)]
+pub enum Period {
+	#[iden = "periods"]
+	Table,
+	Id,
+	Name,
+	From,
+	To,
+}
