1587bdfbd265035d1c2895c5642e591936fc96c0
H3rmt <stemmer.enrico@gmail.com>
2022-10-15 18:32:52 +0200

    added better entity export

---
+++ src/entity/index.ts
@@ -0,0 +1,12 @@
+import {Model as Grade} from "./grade"
+import {Model as Type} from "./type"
+import {Model as Subject} from "./subject"
+import {Model as Period} from "./period"
+
+
+export type {
+	Grade,
+	Type,
+	Subject,
+	Period
+}
