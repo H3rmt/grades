2d22916c48de9e112c3231c4364fcc00ed5dbc48
H3rmt <stemmer.enrico@gmail.com>
2022-12-17 15:58:03 +0100

    removed doubled version + name
    Signed-off-by: H3rmt <stemmer.enrico@gmail.com>

--- src-tauri/src/commands/info.rs
+++ src-tauri/src/commands/info.rs
@@ -7,20 +7,10 @@ use crate::built_info;
 use crate::commands::utils::{CommandError, LogAndString};
 
 #[tauri::command]
-pub async fn get_info_js(app_handle: AppHandle<Wry>) -> Result<String, String> {
-	let version = app_handle.config().package.version.clone().unwrap_or_else(|| {
-		log::warn!("No version found");
-		"version not found".to_string()
-	});
-	
-	let name = app_handle.config().package.product_name.clone().unwrap_or_else(|| {
-		log::warn!("No name found");
-		"name not found".to_string()
-	});
-	
+pub async fn get_info_js() -> Result<String, String> {
 	let info = Info {
-		name,
-		version,
+		name: built_info::PKG_NAME.to_string(),
+		version: built_info::PKG_VERSION.to_string(),
 		authors: built_info::PKG_AUTHORS.to_string(),
 		target: built_info::TARGET.to_string(),
 		profile: built_info::PROFILE.to_string(),
