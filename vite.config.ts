151c40132e173d9205319d32ca774b0dc7a9d36b
H3rmt <stemmer.enrico@gmail.com>
2022-12-17 12:41:58 +0100

    updated vite and added swc
    Signed-off-by: H3rmt <stemmer.enrico@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import {defineConfig} from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -17,8 +17,6 @@ export default defineConfig({
 	// https://tauri.studio/v1/api/config#buildconfig.beforedevcommand
 	envPrefix: ["VITE_", "TAURI_"],
 	build: {
-		// Tauri supports es2021
-		target: ["es2021", "chrome100", "safari13"],
 		// don't minify for debug builds
 		minify: !process.env.TAURI_DEBUG ? "esbuild" : false,
 		// produce sourcemaps for debug builds
