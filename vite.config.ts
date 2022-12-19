285060925d69e0018950b887f0c0bd88819cf474
H3rmt <stemmer.enrico@gmail.com>
2022-12-19 17:58:13 +0100

    replaced manualChunks
    Signed-off-by: H3rmt <stemmer.enrico@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import {defineConfig} from "vite";
 import react from "@vitejs/plugin-react-swc";
-import { splitVendorChunkPlugin } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(),splitVendorChunkPlugin()],
+	plugins: [react()],
+	// https://reactjs.org/docs/code-splitting.html
 
 	// Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
 	// prevent vite from obscuring rust errors
@@ -14,27 +14,21 @@ export default defineConfig({
 		port: 1420,
 		strictPort: true,
 	},
-	// to make use of `TAURI_DEBUG` and other env variables
-	// https://tauri.studio/v1/api/config#buildconfig.beforedevcommand
-	envPrefix: ["VITE_", "TAURI_"],
 	build: {
-		// don't minify for debug builds
-		minify: !process.env.TAURI_DEBUG ? "esbuild" : false,
-		// produce sourcemaps for debug builds
-		sourcemap: !!process.env.TAURI_DEBUG,
-
 		rollupOptions: {
 			output: {
-				// manualChunks: (id) => {
-				// 	if (id.includes("node_modules")) {
-				// 		if (id.includes("@mui")) {
-				// 			return "mui";
-				// 		} else if (id.includes("react")) {
-				// 			return "react";
-				// 		}
-				// 		return "vendor";
-				// 	}
-				// },
+				manualChunks: {
+					'react': [
+						'node_modules/react/index.js',
+						'node_modules/react-dom/index.js'
+					],
+					'query': [
+						'node_modules/@tanstack/react-query/build/lib/index.js',
+					],
+					'mui': [
+						'node_modules/@mui/material/index.js',
+					],
+				},
 			}
 		}
 	},
