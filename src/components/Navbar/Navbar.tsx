dc1dc1536edd478b9b15697d80b248e0636c4750
H3rmt <stemmer.enrico@gmail.com>
2022-12-23 14:50:39 +0100

    replaced Drawer with SwipeableDrawer for mobile
    Signed-off-by: H3rmt <stemmer.enrico@gmail.com>

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,8 +1,12 @@
-import {Divider, Drawer, List, ListItem, ListItemButton, ListItemIcon, ListItemText} from "@mui/material";
+import {Divider, List, ListItem, ListItemButton, ListItemIcon, ListItemText, SwipeableDrawer} from "@mui/material";
 import {Page, Pages} from "../../App";
+import {reactSet} from "../../ts/utils";
 
-const Navbar = (props: { open: boolean, closeNav: () => void, setPage: (page: Page) => void, pages: Pages }) => {
-	return (<Drawer anchor="left" open={props.open} onClose={props.closeNav} variant="temporary">
+const Navbar = (props: { open: boolean, set: reactSet<boolean>, setPage: (page: Page) => void, pages: Pages, openPage: string}) => {
+	const iOS = typeof navigator !== 'undefined' && /iPad|iPhone|iPod/.test(navigator.userAgent);
+
+	return <SwipeableDrawer anchor="left" open={props.open} onOpen={() => props.set(true)} onClose={() => props.set(false)}
+									variant="temporary" disableBackdropTransition={false} disableDiscovery={iOS} swipeAreaWidth={50}>
 		<List disablePadding sx={{height: 1}}>
 			{Object.entries(props.pages).filter(([key]) => key != "settings").map(([key, page]) => (
 					<ListItem key={key} disablePadding>
@@ -12,7 +16,7 @@ const Navbar = (props: { open: boolean, closeNav: () => void, setPage: (page: Pa
 							<ListItemIcon>
 								{page.icon}
 							</ListItemIcon>
-							<ListItemText primary={page.name} secondary={page.description}/>
+							<ListItemText primary={page.name} secondary={page.description} sx={{textDecoration: page.name == props.openPage ? "underline" : ""}}/>
 						</ListItemButton>
 					</ListItem>
 			))}
@@ -28,7 +32,7 @@ const Navbar = (props: { open: boolean, closeNav: () => void, setPage: (page: Pa
 				<ListItemText primary={props.pages.settings.name} secondary={props.pages.settings.description}/>
 			</ListItemButton>
 		</ListItem>
-	</Drawer>);
+	</SwipeableDrawer>;
 };
 
 export default Navbar;
