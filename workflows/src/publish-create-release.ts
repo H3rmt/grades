d4fb3f84ab8c767a98af9fda79c86b3a43e933ef
H3rmt <stemmer.enrico@gmail.com>
2023-01-15 16:49:04 +0100

    fixed target_commitish on publish (to release)
    Signed-off-by: H3rmt <stemmer.enrico@gmail.com>

--- workflows/src/publish-create-release.ts
+++ workflows/src/publish-create-release.ts
@@ -1,5 +1,4 @@
-import {Context} from "@actions/github/lib/context";
-import {Core, Github} from "./type";
+import {Core, Github, Context} from "./type";
 
 export default async ({github, context, core, version, commit_head_message}: {
 	github: Github, context: Context, core: Core, version: string, commit_head_message: string
@@ -14,7 +13,8 @@ export default async ({github, context, core, version, commit_head_message}: {
 		name: `${repo} v${version}`,
 		body: `**_--Draft--_**\n\n### Changes:\n${commit_head_message}\n\nAssets getting generated...`,
 		draft: true,
-		prerelease: false
+		prerelease: false,
+		target_commitish: context.ref_name
 	})
 
 	core.notice(`created release: ${repo} v${version}`)
--- workflows/src/type.ts
+++ workflows/src/type.ts
@@ -1,9 +1,12 @@
 import {GitHub} from "@actions/github/lib/utils";
 import {RestEndpointMethods} from "@octokit/plugin-rest-endpoint-methods/dist-types/generated/method-types";
 import * as C from "@actions/core";
+import {Context as CC} from "@actions/github/lib/context";
 
 export type Github = InstanceType<typeof GitHub> & {
 	rest: RestEndpointMethods;
 }
 
 export type Core = typeof C
+
+export type Context = CC & { ref_name: string }
