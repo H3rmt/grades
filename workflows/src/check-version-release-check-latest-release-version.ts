e42a25fc50b5b532b4dac8cd49ce81dc9284e975
H3rmt <stemmer.enrico@gmail.com>
2023-02-04 17:41:13 +0100

    fixed workflows
    Signed-off-by: H3rmt <stemmer.enrico@gmail.com>

--- workflows/src/check-version-release-check-latest-release-version.ts
+++ workflows/src/check-version-release-check-latest-release-version.ts
@@ -7,12 +7,17 @@ export default async ({github, context, core}: {
 	const owner = context.repo.owner
 	const repo = context.repo.repo
 
-	const {data} = await github.rest.repos.getLatestRelease({
+	const {data} = await github.rest.repos.listReleases({
 		owner: owner,
 		repo: repo,
 	})
 
-	const tag = data.tag_name.replace(`${repo}-v`, '')
+	const release = data.sort((f, r) =>
+			new Date(f.created_at).getUTCMilliseconds() -
+			new Date(r.created_at).getUTCMilliseconds()
+	)[0]
+
+	const tag = release.tag_name.replace(`${repo}-v`, '')
 
 	core.notice(`latest release-tag: ${tag}`)
 
--- workflows/src/publish-release-publish-release.ts
+++ workflows/src/publish-release-publish-release.ts
@@ -1,65 +1,64 @@
-import { Core, Github } from "./type";
-import { Context } from "@actions/github/lib/context";
+import {Core, Github} from "./type"
+import {Context} from "@actions/github/lib/context"
 // noinspection JSUnusedGlobalSymbols
-export default async ({ github, context, core, gistId, fileName }: {
-  github: Github;
-  context: Context;
-  core: Core;
-  gistId: string;
-  fileName: string;
+export default async ({github, context, core, gistId, fileName}: {
+	github: Github;
+	context: Context;
+	core: Core;
+	gistId: string;
+	fileName: string;
 }) => {
-  const owner = context.repo.owner;
-  const repo = context.repo.repo;
+	const owner = context.repo.owner
+	const repo = context.repo.repo
 
-  const { data } = await github.rest.repos.listReleases({
-    owner: owner,
-    repo: repo,
-  });
+	const {data} = await github.rest.repos.listReleases({
+		owner: owner,
+		repo: repo,
+	})
 
-  const release =
-    data.sort((f, r) =>
-      new Date(f.created_at).getUTCMilliseconds() -
-      new Date(r.created_at).getUTCMilliseconds()
-    )[0];
+	const release = data.sort((f, r) =>
+			new Date(f.created_at).getUTCMilliseconds() -
+			new Date(r.created_at).getUTCMilliseconds()
+	)[0]
 
-  await github.rest.repos.updateRelease({
-    owner: owner,
-    repo: repo,
-    release_id: release.id,
-    draft: false,
-    prerelease: false,
-    make_latest: true,
-  });
+	await github.rest.repos.updateRelease({
+		owner: owner,
+		repo: repo,
+		release_id: release.id,
+		draft: false,
+		prerelease: false,
+		make_latest: true,
+	})
 
-  core.info(`set release: ${release.tag_name} as latest`);
+	core.info(`set release: ${release.tag_name} as latest`)
 
-  const update = release.assets.find((asset) => asset.name === "update.json");
-  if (update === undefined) {
-    core.error("update.json not found");
-    return;
-  }
+	const update = release.assets.find((asset) => asset.name === "update.json")
+	if (update === undefined) {
+		core.error("update.json not found")
+		return
+	}
 
-  const asset = await github.rest.repos.getReleaseAsset({
-    owner: owner,
-    repo: repo,
-    asset_id: update.id,
-  });
+	const asset = await github.rest.repos.getReleaseAsset({
+		owner: owner,
+		repo: repo,
+		asset_id: update.id,
+	})
 
-  const response = await github.request(asset.data.browser_download_url, {
-    headers: {
-      accept: "application/octet-stream",
-    },
-  });
+	const response = await github.request(asset.data.browser_download_url, {
+		headers: {
+			accept: "application/octet-stream",
+		},
+	})
 
-  core.info("response DATA");
-  const json = new TextDecoder().decode(response.data);
-  core.info(json);
+	core.info("response DATA")
+	const json = new TextDecoder().decode(response.data)
+	core.info(json)
 
-  core.info(
-    `updating ${fileName} in gist with id: ${gistId} for tauri updater`,
-  );
-  await github.rest.gists.update({
-    gist_id: gistId,
-    files: { [fileName]: { content: json } },
-  });
+	core.info(
+			`updating ${fileName} in gist with id: ${gistId} for tauri updater`,
+	)
+	await github.rest.gists.update({
+		gist_id: gistId,
+		files: {[fileName]: {content: json}},
+	})
 };
