859575e46b51f8821255afbf80ffeab2508dac13
H3rmt <stemmer.enrico@gmail.com>
2023-03-11 00:53:36 +0100

    added multiSubject Support for Analysis
    Signed-off-by: H3rmt <stemmer.enrico@gmail.com>

--- src/Analysis/Analysis.tsx
+++ src/Analysis/Analysis.tsx
@@ -1,54 +1,12 @@
-import {Grid, Typography} from "@mui/material"
-import {AnalysisBox} from "../components/AnalysisBox/AnalysisBox"
-import {Chart} from "../components/Chart/Chart"
-import {ReferenceLine} from "recharts"
-import {useGrades, useNoteRange, useSubjects} from "../commands/get"
-import {useEffect, useState} from "react"
-import {Subject} from "../entity"
-import ReactQueryDataMultiple from "../components/ReactQueryData/ReactQueryDataMultiple"
-import {NoteRange} from "../entity/config"
-import dayjs from "dayjs"
-import SubjectsOverPeriods from "./SubjectsOverPeriods"
+import {Grid, Typography} from '@mui/material'
+import {AnalysisBox} from '../components/AnalysisBox/AnalysisBox'
+import {useGrades, useNoteRange, useSubjects} from '../commands/get'
+import {useEffect, useState} from 'react'
+import {Subject} from '../entity'
+import dayjs from 'dayjs'
+import SubjectsOverPeriods from './SubjectsOverPeriods'
 
 type Props = {}
-
-const data = [
-	{
-		date: 1,
-		uv: 4000,
-		pv: 2400,
-	},
-	{
-		date: 2,
-		uv: 3000,
-		pv: 1398,
-	},
-	{
-		date: 3,
-		uv: 2000,
-		pv: 9800,
-	},
-	{
-		date: 4,
-		uv: 2780,
-		pv: 3908,
-	},
-	{
-		date: 5,
-		uv: 1890,
-		pv: 4800,
-	},
-	{
-		date: 6,
-		uv: 2390,
-		pv: 3800,
-	},
-	{
-		date: 7,
-		uv: 3490,
-		pv: 4300,
-	},
-]
 
 export default function Component(props: Props) {
 	const [subjects, , subjectsS] = useSubjects()
@@ -60,20 +18,6 @@ export default function Component(props: Props) {
 	const [subjectData, setSubjectData] = useState<unknown | null>(null)
 
 	const [grades, , gradesS] = useGrades()
-
-	useEffect(() => {
-		if (subject === null || grades === undefined)
-			return
-
-		setSubjectData(grades
-				.filter(grade => grade.subject === subject.id)
-				.filter(grade => grade.grade !== null) // @ts-ignore
-				.sort((a, b) => dayjs(a.date, 'DD-MM-YYYY').diff(dayjs(b.date, 'DD-MM-YYYY')) < 0)
-				.map(grade => ({
-					date: grade.date,
-					grade: grade.grade
-				})))
-	}, [subject, grades])
 
 	return (<Grid container spacing={2} padding={2}>
 				<Grid item xs={12} sm={12} md={12} xl={6}>
@@ -100,7 +44,7 @@ export default function Component(props: Props) {
 							FEF FEF FEF
 						</Typography>
 					]}>
-						<Chart data={data} lines={[{
+						{/*<Chart data={data} lines={[{
 							name: 'uv',
 							color: 'red'
 						}, {
@@ -108,7 +52,7 @@ export default function Component(props: Props) {
 							color: 'orange'
 						}]} extra={
 							<ReferenceLine y={6000} label="Avg"/>
-						}></Chart>
+						}></Chart>*/}
 					</AnalysisBox>
 				</Grid>
 			</Grid>
--- src/Analysis/SubjectsOverPeriods/index.tsx
+++ src/Analysis/SubjectsOverPeriods/index.tsx
@@ -1,45 +1,98 @@
-import {useGrades, useNoteRange, usePeriods, useSubjects} from "../../commands/get"
-import {Grade, Period, Subject} from "../../entity"
-import {useEffect, useState} from "react"
-import ReactQueryDataMultiple from "../../components/ReactQueryData/ReactQueryDataMultiple"
-import {NoteRange} from "../../entity/config"
-import {AnalysisBox} from "../../components/AnalysisBox/AnalysisBox"
-import {Typography} from "@mui/material"
-import {Chart} from "../../components/Chart/Chart"
+import {useGrades, useNoteRange, usePeriods, useSubjects, useTypes, useWeights} from '../../commands/get'
+import {Grade, Period, Subject} from '../../entity'
+import {useEffect, useState} from 'react'
+import ReactQueryDataMultiple from '../../components/ReactQueryData/ReactQueryDataMultiple'
+import {NoteRange} from '../../entity/config'
+import {AnalysisBox} from '../../components/AnalysisBox/AnalysisBox'
+import {Typography} from '@mui/material'
+import {Chart, Data} from '../../components/Chart/Chart'
 
 export default function SubjectsOverPeriods() {
 	const [subjects, , subjectsS] = useSubjects()
 
+	const [types, , typesS] = useTypes()
+
 	const [noteRange, , noteRangeS] = useNoteRange()
+
+	const [weights, , weightsS] = useWeights()
 
 	const [periods, , periodsS] = usePeriods()
 
-	const [subject, setSubject] = useState<Subject | null>(null)
+	const [subjectsSelected, setSubjectsSelected] = useState<Subject[]>([])
 
-
-	// [{x: "New Period", value: 7.25}, {x: "New Period 1", value: 6.5}]
-	const [data, setData] = useState<unknown | null>([{x: "New Period", value: 7.25}, {x: "New Period 1", value: 6.5}])
+	const [data, setData] = useState<Data[]>([])
 
 	const [grades, , gradesS] = useGrades()
 
 	useEffect(() => {
-		if (subject === null || grades === undefined || periods === undefined)
+		if (subjectsSelected.length === 0 || grades === undefined || periods === undefined || weights === undefined || subjects === undefined || types === undefined)
 			return
 
-		const split: Grade[][] = []
+		// subject: [period: [grade]]
+		const split: Grade[][][] = []
 
-		grades.filter(grade => grade.subject === subject.id)
-				.forEach(grade => split[grade.period] ? split[grade.period].push(grade) : (split[grade.period] = [grade]))
+		grades.filter(grade => subjectsSelected.find(s => s.id === grade.subject) !== undefined)
+				.filter(grade => grade.grade !== null)
+				.forEach(grade =>
+						split[grade.subject] ?
+								split[grade.subject][grade.period] ?
+										split[grade.subject][grade.period].push(grade) :
+										(split[grade.subject][grade.period] = [grade]) :
+								(split[grade.subject] = Array(grade.period + 1).fill(0).map((_, i) => i === grade.period ? [grade] : []))
+				)
 
-		const data = []
+		// console.info(split)
 
-		split.map(period => ({
-			x: periods.find(p => p.id == period[0].period)?.name,
-			value: period.reduce((a, b) => a + (b.grade ?? 0), 0) / period.length
-		})).forEach(d => data.push(d))
+		const data: Data[] = periods
+				.map(period => {
+					const d: { [key: string]: number } = {}
+
+					subjectsSelected.map(subject => {
+						const grades = split[subject.id]?.[period.id]
+						if (grades === undefined)
+							return
+						// console.log('')
+						// console.log(grades.length)
+						const gradescount = grades
+								.filter(grade => grade.type !== types.find(t => t.name === 'Klausur')?.id)
+								.reduce((a, b) => {
+									for (const weight of weights) {
+										if (weight.name == b.weight)
+											return a + Number(weight.value)
+									}
+									return a + 1 // one weight does always match
+								}, 0)
+						// console.log(gradescount)
+						const gradesum = grades
+								.filter(grade => grade.type !== types.find(t => t.name === 'Klausur')?.id)
+								.reduce((a, b) => {
+									for (const weight of weights) {
+										if (weight.name == b.weight)
+											return a + (b.grade ?? 0) * Number(weight.value)
+									}
+									return a + 1 // one weight does always match
+								}, 0)
+						// console.log(gradesum)
+
+						const klausur = grades.find(grade => grade.type === types.find(t => t.name === 'Klausur')?.id)
+						// console.log(klausur)
+
+						// const final = ((gradescount !== 0 ? Math.round(gradesum / gradescount) : 0) + (klausur?.grade ?? 0)) / ((klausur !== undefined && gradescount !== 0) ? 2 : 1)
+						const final = ((gradescount !== 0 ? (gradesum / gradescount) : 0) + (klausur?.grade ?? 0)) / ((klausur !== undefined && gradescount !== 0) ? 2 : 1)
+
+						if (gradescount !== 0 || klausur !== undefined)
+							d[subject.name] = Math.round((final + Number.EPSILON) * 100) / 100
+					})
+
+					return {
+						x: period.name,
+						data: d
+					}
+				})
+				.sort((a, b) => a.x.localeCompare(b.x))
 
 		setData(data)
-	}, [subject, grades, periods])
+	}, [subjectsSelected, subjects, grades, periods])
 
 	return <ReactQueryDataMultiple<[Subject[], NoteRange, Period[]]>
 			queries={[
@@ -51,20 +104,23 @@ export default function SubjectsOverPeriods() {
 				const subjects = d[0]
 				const noteRange = d[1]
 
-				console.log(data)
-
-				return <AnalysisBox title="Compare Subjects" top={
+				return <AnalysisBox title="Subjects over Periods" top={
 					subjects.map((subject, index) =>
 							<Typography onClick={() => {
-								setSubject(subject)
+								if (subjectsSelected.find(s => s.id === subject.id) !== undefined)
+									setSubjectsSelected(subjectsSelected.filter(s => s.id !== subject.id))
+								else
+									setSubjectsSelected([...subjectsSelected, subject])
 							}} key={index} variant="h6" sx={{color: subject.color}}>
 								{subject.name}
 							</Typography>)
-				}>{subject !== null && data !== null &&
-						<Chart YAxisDomain={[noteRange.from, noteRange.to]} data={data} lines={[{
-							name: 'value',
-							color: subject.color
-						}]} height={400} width={600}></Chart>
+				}>{subjects.length !== 0 && data !== null &&
+						<Chart YAxisDomain={[noteRange.from, noteRange.to]} data={data} lines={
+							subjectsSelected.map(subject => ({
+								name: subject.name,
+								color: subject.color,
+							}))
+						} height={400} width={600}></Chart>
 				}
 				</AnalysisBox>
 			}
